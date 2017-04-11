23ef0cbce0fe6e0c7faf36a7de57eed7da1479d9
redligot2009 <redligot2009.forreal@gmail.com>
2017-04-11 23:36:57 +0800

    Updated solution

--- HS12MBR.cpp
+++ HS12MBR.cpp
@@ -1,74 +1,86 @@
+// CompetitiveProgramming.cpp : Defines the entry point for the console application.
+//
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	// your code goes here
-	int T;
-	cin >> T;
-	while(T--)
-	{
-	    int n;
-	    int lowerLeftX = INT_MAX, lowerLeftY = INT_MAX, upperRightX = INT_MIN, upperRightY = INT_MIN;
-	    cin >> n;
-	    while(n--)
-	    {
-	        char c;
-	        cin >> c;
-	        int x1, x2, y1, y2, r;
-	        if(c == 'c')
-	        {
-	            cin >> x1 >> y1 >> r;
-	            lowerLeftX = min(x1-r, lowerLeftX);
-	            lowerLeftY = min(y1-r, lowerLeftY);
-	            upperRightX = max(x1+r, upperRightX);
-	            upperRightY = max(y1+r, upperRightY);
-	        }
-	        else if(c == 'p')
-	        {
-	            cin >> x1 >> y1;
-	            lowerLeftX = min(x1, lowerLeftX);
-	            lowerLeftY = min(y1, lowerLeftY);
-	            upperRightX = max(lowerLeftX, upperRightX);
-	            upperRightY = max(lowerLeftY, upperRightY);
-	        }
-	        else if(c == 'l')
-	        {
-	            cin >> x1 >> y1 >> x2 >> y2;
-	            if(x1 < x2)
-	            {
-	                lowerLeftX = min(x1, lowerLeftX);
-	            }
-	            else
-	            {
-	                lowerLeftX = min(x2, lowerLeftX);
-	            }
-	            if(y1 < y2)
-	            {
-	                lowerLeftY = min(y1, lowerLeftY);
-	            }
-	            else
-	            {
-	                lowerLeftY = min(y2, lowerLeftY);
-	            }
-	            if(x1 > x2)
-	            {
-	                upperRightX = max(x1, upperRightX);
-	            }
-	            else
-	            {
-	                upperRightX = max(x2, upperRightX);
-	            }
-	            if(y1 > y2)
-	            {
-	                upperRightY = max(y1, upperRightY);
-	            }
-	            else
-	            {
-	                upperRightY = max(y2, upperRightY);
-	            }
-	        }
-	    }
-	    cout << lowerLeftX << " " << lowerLeftY << " " << upperRightX << " " << upperRightY << endl;
-	}
-	return 0;
+struct Point
+{
+	int x, y;
+	Point(int x, int y);
+};
+
+Point::Point(int x = 0, int y = 0)
+{
+	this->x = x;
+	this->y = y;
 }
+
+struct Rectangle
+{
+	Point bottomLeft;
+	Point topRight;
+	Rectangle(Point bottomLeft, Point topRight);
+	void Update(int x, int y);
+};
+
+Rectangle::Rectangle(Point bottomLeft, Point topRight)
+{
+	this->bottomLeft = bottomLeft;
+	this->topRight = topRight;
+}
+
+void Rectangle::Update(int x, int y)
+{
+	this->bottomLeft.x = min(x, this->bottomLeft.x);
+	this->bottomLeft.y = min(y, this->bottomLeft.y);
+	this->topRight.x = max(x, this->topRight.x);
+	this->topRight.y = max(y, this->topRight.y);
+}
+
+int main()
+{
+	//freopen("in.txt", "r", stdin);
+	//freopen("out.txt", "w", stdout);
+	std::ios_base::sync_with_stdio(false);
+	int t;
+	char shape;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		Rectangle plane = Rectangle(Point(1000, 1000), Point(-1000, -1000));
+		while (n--)
+		{
+			cin >> shape;
+			if (shape == 'p')
+			{
+				int x, y;
+				cin >> x >> y;
+				plane.Update(x, y);
+			}
+			else if (shape == 'l')
+			{
+				int x1, x2;
+				int y1, y2;
+				cin >> x1 >> y1 >> x2 >> y2;
+				plane.Update(x1, y1);
+				plane.Update(x2, y2);
+			}
+			else
+			{
+				int x, y;
+				cin >> x >> y;
+				int r;
+				cin >> r;
+				plane.Update(x + r, y + r);
+				plane.Update(x - r, y - r);
+			}
+		}
+		cout << plane.bottomLeft.x << " " << plane.bottomLeft.y << " " << plane.topRight.x << " " << plane.topRight.y << endl;
+	}
+	//fclose(stdin);
+	//fclose(stdout);
+    return 0;
+}
+
